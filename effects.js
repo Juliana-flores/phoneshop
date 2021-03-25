b64cd996f0c3731af446cd6007966433d38f1b5c
Juliana Flores <floresjuliana@live.com>
2021-03-25 18:09:22 -0300

    edit

--- effects.js
+++ effects.js
@@ -1,6 +1,6 @@
 const phrase =
     '"O melhor lugar de fones é aqui! Tudo o que você precisa, você encontra."';
-const element = document.querySelector(".mensagem");
+const element = document.querySelector(".site-title");
 for (let i = 0; i < phrase.length; i++) {
     setTimeout(() => {
         element.innerHTML += phrase[i];
