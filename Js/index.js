0fca281a8366f01ddae1ba912e6fd158960cc477
Juliana Flores <floresjuliana@live.com>
2021-04-03 17:48:38 -0300

    mudancas

---
+++ Js/index.js
@@ -0,0 +1,11 @@
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i)=> {
+        setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+
+}
+
+const titulo = document.getElementById('corre');
+typeWrite(titulo);
