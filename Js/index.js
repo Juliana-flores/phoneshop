d1ed98281e0d998639eb217a1dbd949507a1fc0f
Juliana Flores <floresjuliana@live.com>
2021-05-02 09:59:07 -0300

    atualizado

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,4 @@
+//Função para fazer o efeito das letras
 function typeWrite(elemento){
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
@@ -6,6 +7,19 @@ function typeWrite(elemento){
     });
 
 }
-
+//Função para fazer o favorito ficar com cor
 const titulo = document.getElementById('corre');
 typeWrite(titulo);
+
+function someFunction (event) {
+    const element = event.target;
+    const className = element.className;
+    
+    if (className.includes('fa-heart') && !className.includes('active')) {
+      element.classList.add('active')
+    } else if (className.includes('fa-heart') && className.includes('active')) {
+      element.classList.remove('active')
+    }
+  }
+  
+  window.addEventListener('click', someFunction, false);
