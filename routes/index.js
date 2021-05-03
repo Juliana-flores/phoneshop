caf941b9cc2651b054270bd5560d3f0121833c76
DiegoBastosCosta <dbdcosta96@gmail.com>
2021-05-03 20:29:46 -0300

    Adicionando rota de contato e correções no código

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const router = express.Router()
-const products = require('../data/products.json')
+const Product = require('../models/product')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.marko(require('../views/index.marko'), {
-    products
+  Product.find((error, products) => {
+    res.marko(require('../views/index.marko'), {
+      products
+    })
   })
 })
 
 router.use(require('./login'))
 router.use(require('./about'))
 router.use(require('./cadastro'))
+router.use(require('./contato'))
 
 module.exports = router
